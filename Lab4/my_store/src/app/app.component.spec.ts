3edf4117d65206350c2af0f08bfa22277ffafc15
Sisalanatra <a_ten@kbtu.kz>
2023-02-22 21:36:46 +0600

    kaspi

--- Lab4/my_store/src/app/app.component.spec.ts
+++ Lab4/my_store/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-caspi-app'`, () => {
+  it(`should have as title 'my-kaspi-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
   });
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-caspi-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-kaspi-app app is running!');
   });
 });
