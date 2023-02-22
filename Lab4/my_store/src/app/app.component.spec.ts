97db126bc6090ad76d5b30ba94b9b51b0eca34ee
Sisalanatra <a_ten@kbtu.kz>
2023-02-22 21:34:52 +0600

    kaspi

--- Lab4/my_store/src/app/app.component.spec.ts
+++ Lab4/my_store/src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'my-caspi-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-caspi-app');
   });
 
   it('should render title', () => {
